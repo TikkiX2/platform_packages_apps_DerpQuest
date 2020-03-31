5eb949220fece77ca9af9772c14e8b593fb64a97
TikkiTikki <radstylemusic@gmail.com>
2020-03-31 03:38:04 -0300

    Introduce RGB gradient [2/2]

--- src/com/derpquest/settings/fragments/ThemeSettings.java
+++ src/com/derpquest/settings/fragments/ThemeSettings.java
@@ -64,6 +64,8 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
     private static final String ACCENT_PRESET = "accent_preset";
     private static final String ACCENT_COLOR = "accent_color";
     private static final String ACCENT_COLOR_PROP = "persist.sys.theme.accentcolor";
+    private static final String GRADIENT_COLOR = "gradient_color";
+    private static final String GRADIENT_COLOR_PROP = "persist.sys.theme.gradientcolor";
     private static final String SYSUI_ROUNDED_SIZE = "sysui_rounded_size";
     private static final String SYSUI_ROUNDED_CONTENT_PADDING = "sysui_rounded_content_padding";
     private static final String SYSUI_STATUS_BAR_PADDING = "sysui_status_bar_padding";
@@ -73,6 +75,7 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
     private IOverlayManager mOverlayService;
     private ListPreference mAccentPreset;
     private ColorPickerPreference mThemeColor;
+    private ColorPickerPreference mGradientColor;
     private CustomSeekBarPreference mCornerRadius;
     private CustomSeekBarPreference mContentPadding;
     private CustomSeekBarPreference mSBPadding;
@@ -111,6 +114,14 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
             mThemeColor.setNewPreviewColor(Color.WHITE);
         }
         mThemeColor.setOnPreferenceChangeListener(this);
+
+        mGradientColor = (ColorPickerPreference) findPreference(GRADIENT_COLOR);
+        String colorVal = SystemProperties.get(GRADIENT_COLOR_PROP, "-1");
+        int color = "-1".equals(colorVal)
+                ? Color.WHITE
+                : Color.parseColor("#" + colorVal);
+        mGradientColor.setNewPreviewColor(color);
+        mGradientColor.setOnPreferenceChangeListener(this);
 
         mAccentPreset = (ListPreference) findPreference(ACCENT_PRESET);
         mAccentPreset.setOnPreferenceChangeListener(this);
@@ -176,6 +187,16 @@ public class ThemeSettings extends SettingsPreferenceFragment implements
                  mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
             } catch (RemoteException ignored) {
             }
+        } else if (preference == mGradientColor) {
+            int color = (Integer) newValue;
+            String hexColor = String.format("%08X", (0xFFFFFFFF & color));
+            SystemProperties.set(GRADIENT_COLOR_PROP, hexColor);
+            try {
+                 mOverlayService.reloadAndroidAssets(UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.settings", UserHandle.USER_CURRENT);
+                 mOverlayService.reloadAssets("com.android.systemui", UserHandle.USER_CURRENT);
+             } catch (RemoteException ignored) {
+             }
         } else if (preference == mAccentPreset) {
             String value = (String) newValue;
             int index = mAccentPreset.findIndexOfValue(value);
